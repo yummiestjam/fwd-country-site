722b0f7f86eb3f3d8d069f8e784e57e14e04dcd9
Jamie Teng <yummy yummy @gmail.com>
2025-02-05 18:30:26 -0800

    tweaked menu graphics

--- scripts/nav-menu-toggle.js
+++ scripts/nav-menu-toggle.js
@@ -1,9 +1,29 @@
 
-
+// variables for menu toggle
 const button = document.getElementById('nav-button');
 const list = document.getElementById('nav-list');
 
+// variables for icon switching
+const svgOpen = document.getElementById('open-menu-svg');
+const svgClose = document.getElementById('close-menu-svg');
+
+svgClose.style.display = 'none';
+
 button.addEventListener('click', function(){
+    // toggle menu
     list.classList.toggle('toggled');
+
+    // switch icons
+    if (list.classList.contains('toggled')) {
+        svgClose.style.display = 'block';
+        svgOpen.style.display = 'none';
+        button.style.backgroundColor = '#09121a';
+    }
+    else {
+        svgClose.style.display = 'none';
+        svgOpen.style.display = 'block';
+        button.style.backgroundColor = '#FFFFFF';
+    }
 })
+
 
